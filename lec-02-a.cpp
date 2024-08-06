5a70a57bc5e40f10fcdafc9339b0e948b8b0fe8e
Muhammad Saad <149876788+MuhammadSaad38@users.noreply.github.com>
2024-08-06 13:36:12 +0500

    learning encapsulation concept

---
+++ lec-02-a.cpp
@@ -0,0 +1,16 @@
+#include <iostream>          //Encapsulation Basically Used wrapping a data &
+#include <string>            // member function in a single unit called class 
+using namespace std;
+
+class Accounts {
+private:
+    double balance; // <--data hiding concept 
+    string password;
+public:
+    string accountId;
+    string userName;
+};
+int main(){
+    
+    return 0;
+}
